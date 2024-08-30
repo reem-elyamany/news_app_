8cd803b83a8c60b093c34aa614e03731e5f365fe
reem-elyamany <ReemSalman652002@gmail.com>
2024-08-30 19:18:21 +0300

    first commit

---
+++ lib/services/news_services.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:dio/dio.dart';
+import 'package:news_app/models/article_model.dart';
+
+class NewsService {
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  Future<List<ArticleModel>> getTopHeadlines({required String category}) async {
+    try {
+      var response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=0bb34f3ed51342a2bcab1c5be6abc3a8&category=$category');
+
+      Map<String, dynamic> jsonData = response.data;
+
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articlesList = [];
+
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          subTitle: article['description'],
+        );
+        articlesList.add(articleModel);
+      }
+
+      return articlesList;
+    } catch (e) {
+      return [];
+    }
+  }
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/widgets/category_list_view.dart';
+import 'package:news_app/widgets/news_lis_view_builder.dart';
+
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(child: CategoriesListView()),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 32,
+              ),
+            ),
+            NewsListViewBuilder(
+              category: 'general',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
